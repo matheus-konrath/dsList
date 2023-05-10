1d784f029d75b123628d7f7257c07c658cfdd93e
Matheus Konrath <mkonrath96@gmail.com>
2023-05-10 19:33:08 -0300

    start project

---
+++ dslist/src/main/java/com/devsuperior/dslist/entities/GameEntity.java
@@ -0,0 +1,30 @@
+package com.devsuperior.dslist.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Data
+@Entity
+@Table(name = "tb_game")
+public class GameEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    @Column(name = "game_year")
+    private Integer year;
+    private String genre;
+    private String platforms;
+    private Double score;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+
+}
